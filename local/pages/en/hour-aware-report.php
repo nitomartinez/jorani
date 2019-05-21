f5b2cd2b40b12cdf3b294fff179cce7354c794a0
Román Puga Quintairos <romanallariz@gmail.com>
2019-05-21 08:57:51 +0200

    Refactor SQL functions

--- local/pages/en/hour-aware-report.php
+++ local/pages/en/hour-aware-report.php
@@ -12,290 +12,6 @@ require_once FCPATH . "vendor/autoload.php";
 
 $this->load->model('organization_model');
 
-function getLeaveTypes($ci) {
-$sql = <<<EOF
--- 
--- The list of leave types
-
-select id,name
-  from types
- order by id
-;
-EOF;
-
-    $query = $ci->db->query($sql);
-
-    $rows = $query->result_array();
-    $ids = array();
-    foreach($rows as $row) {
-        $ids[$row["id"]] = $row["name"];
-    }
-    return $ids;
-}
-
-
-function getUserIdsWithLeavesBetweenDates($ci, $startdate, $enddate) {
-
-    $sql = <<<EOF
--- 
--- The list of reports are the reports is the union of the following reports
--- - leaves that are between the start and the end date
--- - leaves that are start before the start date and end between the start and the end date
--- - leaves that start between the start and end date and end after the end date
-
-select distinct users.id 
- from 
-      leaves
- join status on leaves.status = status.id
- join users on leaves.employee = users.id
- join types on leaves.type = types.id
-where
-      status.id = 3 AND -- ACCEPTED
-      ( (
-          leaves.startdate >= '$startdate' AND
-          leaves.enddate  <= '$enddate'
-        ) OR 
-        (
-          leaves.startdate < '$startdate' AND
-          leaves.enddate  >= '$startdate' AND
-          leaves.enddate  <= '$enddate'
-        ) OR
-        (
-          leaves.startdate >= '$startdate' AND
-          leaves.startdate  <= '$enddate' AND
-          leaves.enddate > '$enddate'
-        )
-      )
-order by users.id
-EOF;
-
-    $query = $ci->db->query($sql);
-
-    $rows = $query->result_array();
-    $ids = array();
-    foreach($rows as $row) {
-        $ids[] = $row["id"];
-    }
-    return $ids;
-}
-
-
-function getLeavesBetweenDates($ci, $startdate, $enddate) {
-
-    $tablename = str_replace("-", "_", "leavestable_${startdate}_${enddate}");
-
-    
-    $createtemptable = <<<EOF
-create or replace temporary table $tablename
--- 
--- The list of reports are the reports is the union of the following reports
--- - leaves that are between the start and the end date
--- - leaves that are start before the start date and end between the start and the end date
--- - leaves that start between the start and end date and end after the end date
-
-select t.userid, t.firstname, t.lastname, t.type, t.leavetype, SUM(t.effectiveduration) as duration from
-(
-(
-select leaves.id as id,
-       users.id as userid,
-       users.firstname,
-       users.lastname,
-       leaves.type,
-       types.name as leavetype,
-       status.name as leavestatus,
-       leaves.startdate,
-       leaves.enddate,
-       leaves.duration,
-       @durationoutsideofperiodv := 0 as durationoutsideofperiod, 
-       @daysoffoutsideofperiodv := 0 as daysoffoutsideofperiod,
-       @halfdayadjustment := 0 as halfdayadjustment,
-       (leaves.duration - @durationoutsideofperiodv + @daysoffoutsideofperiodv + @halfdayadjustment) as effectiveduration
- from 
-      leaves
- join status on leaves.status = status.id
- join users on leaves.employee = users.id
- join types on leaves.type = types.id
-where
-      status.id = 3 AND -- ACCEPTED
-      leaves.startdate >= '$startdate' AND
-      leaves.enddate  <= '$enddate'
-) UNION
-(
--- effectiveduration for leaves that start between '$startdate' and '$enddate':
--- The duration of the leave
--- Substracting the days after the end date (except the daysoff)
--- And adjusting half a day in case the end was in the Morning
--- Example:
---  '$startdate' = "2019-04-01";
---  '$enddate' = "2019-04-30";
---
--- Calendar:
---
---      March 2019           April           
--- Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa 
---                 1  2      1  2  3  4  5  6
---  3  4  5  6  7  8  9   7  8  9 10 11 12 13
--- 10 11 12 13 14 15 16  14 15 16 17 18 19 20
--- 17 18 19 20 21 22 23  21 22 23 24 25 26 27
--- 24 25 26 27 28 29 30  28 29 30
--- 31
--- 
--- Leave:
--- Starting 2019-03-28 in the Afternoon and ending 2019-04-01 in the morning
--- +----+------------+------------+--------+----------+-------+---------------+-------------+----------+------+
--- | id | startdate  | enddate    | status | employee | cause | startdatetype | enddatetype | duration | type |
--- +----+------------+------------+--------+----------+-------+---------------+-------------+----------+------+
--- | 20 | 2019-03-28 | 2019-04-01 |      3 |        1 |       | Afternoon     | Morning     |    2.000 |    1 |
--- +----+------------+------------+--------+----------+-------+---------------+-------------+----------+------+
--- 
--- The duration for the period is 0.5 days, where
--- duration = 2 (from the leave output above) 
---            - 4 (days from 28th of March until the 31st of March Mar, these are durationoutsideofperiodv)
---            + 2 (dayoffs, 30th and 31st of Mar, Saturday and Sunday)
---            + 0.5 (as startdatetype is Afternon, if it would be in the Morning the duration in the leave would have been 2.5 days)
-select leaves.id as id,
-       users.id as userid,
-       users.firstname,
-       users.lastname, 
-       leaves.type,
-       types.name as leavetype,
-       status.name as leavestatus,
-       leaves.startdate,
-       leaves.enddate,
-       leaves.duration,
-       @durationoutsideofperiodv := DATEDIFF('$startdate', leaves.startdate) as durationoutsideofperiod, 
-       @daysoffoutsideofperiodv := (select count(1) 
-                                     from dayoffs
-                                    where
-                                          dayoffs.contract = users.contract and
-                                          dayoffs.date < '$startdate' and
-                                          dayoffs.date >= leaves.startdate) as daysoffoutsideofperiod,
-       @halfdayadjustment := if(startdatetype = "Afternoon", 0.5, 0) as halfdayadjustment,
-       (leaves.duration - @durationoutsideofperiodv + @daysoffoutsideofperiodv + @halfdayadjustment) as effectiveduration
- from 
-      leaves
- join status on leaves.status = status.id
- join users on leaves.employee = users.id
- join types on leaves.type = types.id
-where
-      status.id = 3 AND -- ACCEPTED
-      leaves.startdate < '$startdate' AND
-      leaves.enddate  >= '$startdate' AND
-      leaves.enddate  <= '$enddate'
-) UNION
-(
--- effectiveduration for leaves that start between '$startdate' and '$enddate':
--- The duration of the leave
--- Substracting the days after the end date (except the daysoff)
--- And adjusting half a day in case the end was in the Morning
--- Example:
---  '$startdate' = "2019-04-01";
---  '$enddate' = "2019-04-30";
---
--- Calendar:
---
---       April                  May           
--- Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa 
---     1  2  3  4  5  6            1  2  3  4 
---  7  8  9 10 11 12 13   5  6  7  8  9 10 11 
--- 14 15 16 17 18 19 20  12 13 14 15 16 17 18 
--- 21 22 23 24 25 26 27  19 20 21 22 23 24 25 
--- 28 29 30              26 27 28 29 30 31    
---
--- Leave:
--- Starting 2019-04-30 in the Afternoon and ending 2019-05-02 in the morning
--- +----+------------+------------+--------+----------+-------+---------------+-------------+----------+------+
--- | id | startdate  | enddate    | status | employee | cause | startdatetype | enddatetype | duration | type |
--- +----+------------+------------+--------+----------+-------+---------------+-------------+----------+------+
--- | 23 | 2019-04-30 | 2019-05-06 |      3 |        1 |       | Afternoon     | Morning     |    4.000 |    1 |
--- +----+------------+------------+--------+----------+-------+---------------+-------------+----------+------+
--- 
--- The duration for the period is 0.5 days, where
--- duration = 4 (from the leave output above) 
---            - 6 (days from 1st of May until the 6th of May, these are durationoutsideofperiodv)
---            + 2 (dayoffs, 4th and 5th of May which are Saturday and Sunday)
---            + 0.5 (as enddatetype is Morning, if it would be in the Afternoon the duration in the leave would have been 4.5 days)
-select leaves.id as id,
-       users.id as userid,
-       users.firstname,
-       users.lastname,
-       leaves.type,
-       types.name as leavetype,
-       status.name as leavestatus,
-       leaves.startdate,
-       leaves.enddate,
-       leaves.duration,
-       @durationoutsideofperiodv := DATEDIFF(leaves.enddate, '$enddate') as durationoutsideofperiod, 
-       @daysoffoutsideofperiodv := (select count(1) 
-                                     from dayoffs
-                                    where
-                                          dayoffs.contract = users.contract and
-                                          dayoffs.date > '$enddate' and
-                                          dayoffs.date <= leaves.enddate) as daysoffoutsideofperiod,
-       @halfdayadjustment := if(enddatetype = "Morning", 0.5, 0) as halfdayadjustment,
-       (leaves.duration - @durationoutsideofperiodv + @daysoffoutsideofperiodv + @halfdayadjustment) as effectiveduration
- from 
-      leaves
- join status on leaves.status = status.id
- join users on leaves.employee = users.id
- join types on leaves.type = types.id
-where
-      status.id = 3 AND -- ACCEPTED
-      leaves.startdate >= '$startdate' AND
-      leaves.startdate  <= '$enddate' AND
-      leaves.enddate > '$enddate'
-)  
-) t
-group by t.userid, t.type
-EOF;
-
-    $querystart = <<<EOF2
-select userid,firstname,lastname
-EOF2;
-    $queryend = <<<EOF3
-  , SUM(duration) as total
-  from $tablename
-  group by userid
-EOF3;
-
-    $leavetypes = getLeaveTypes($ci);
-    $query = $querystart;
-    foreach ($leavetypes as $id => $type) {
-        $query .= <<<EOF4
-        ,sum(coalesce(
-         (select duration
-          where type = $id)
-       ,0)) as '$type'
-EOF4;
-    }
-    
-
-    $query .= $queryend;
-    if ($GLOBALS['debug']) {
-        echo nl2br($createtemptable);
-        echo "<br/>";
-    }
-
-    $droptemptable = "drop temporary table $tablename";
-
-    $createoutput = $ci->db->query($createtemptable);
-    if ($GLOBALS['debug'])
-        echo nl2br($query);
-
-    $queryresult = $ci->db->query($query);
-    $queryresult2 = $ci->db->query("select * from $tablename");
-    if ($GLOBALS['debug']) {
-        echo "error:".print_r($ci->db->error());
-         echo "<br/>";
-    }
-    $rows2 = $queryresult2->result_array();
-    $rows = $queryresult->result_array();
-    $dropoutput = $ci->db->query($droptemptable);
-
-    return $rows2;
-}
-
-
 ?>
 
 <h2><?php echo lang('reports_leaves_title');?></h2>
@@ -319,9 +35,6 @@ EOF4;
     </div>
     <div class="span3">
         <div class="pull-right">
-            <label for="chkLeaveDetails">
-                    <input type="checkbox" id="chkLeaveDetails" name="chkLeaveDetails" /> <?php echo lang('reports_leaves_field_leave_requests');?>
-            </label>
             &nbsp;
             <button class="btn btn-primary" id="cmdLaunchReport"><i class="mdi mdi-file-chart"></i>&nbsp; <?php echo lang('reports_leaves_button_launch');?></button>
             <button class="btn btn-primary" id="cmdExportReport"><i class="mdi mdi-download"></i>&nbsp; <?php echo lang('reports_leaves_button_export');?></button>
@@ -397,7 +110,7 @@ $(document).ready(function() {
     $('#enddate').datepicker('setDate', 1);
 
     $('#cmdExportReport').click(function() {
-        var rtpQuery = '<?php echo base_url();?>reports/leaves/exportDates';
+        var rtpQuery = '<?php echo base_url();?>hour-aware-report-export';
         var tmpUnix = moment($("#refdate").datepicker("getDate")).utc().unix();
         var startdate = $("#startdate").val();
         var enddate = $("#enddate").val();
@@ -409,6 +122,7 @@ $(document).ready(function() {
 
         if (startdate == 0 || enddate == 0) {
                 bootbox.alert("<?php echo lang('leaves_flash_msg_hourly_reports_days_report');?>");
+                return false;
         }
         rtpQuery += '&startdate=' + startdate;
         rtpQuery += '&enddate=' + enddate;
@@ -426,8 +140,7 @@ $(document).ready(function() {
     });
 
     $('#cmdLaunchReport').click(function() {
-        var ajaxQuery = '<?php echo base_url();?>reports/leaves/executeDates';
-        var tmpUnix = moment($("#refdate").datepicker("getDate")).utc().unix();
+        var ajaxQuery = '<?php echo base_url();?>hour-aware-report-show';
         var startdate = $("#startdate").val();
         var enddate = $("#enddate").val();
         if (entity != -1) {
@@ -438,6 +151,7 @@ $(document).ready(function() {
 
         if (startdate == 0 || enddate == 0) {
                 bootbox.alert("<?php echo lang('leaves_flash_msg_hourly_reports_days_report');?>");
+                return false;
         }
 
         ajaxQuery += '&startdate=' + startdate;
