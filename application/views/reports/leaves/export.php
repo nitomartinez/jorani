dd0921496546cab6bff9be17afd41828daeb2103
Román Puga Quintairos <romanallariz@gmail.com>
2019-05-21 12:17:54 +0200

    Update row's data export

--- application/views/reports/leaves/export.php
+++ application/views/reports/leaves/export.php
@@ -22,49 +22,92 @@ $sheet = $spreadsheet->getActiveSheet();
 
 $sheet->setTitle(mb_strimwidth(lang('reports_export_leaves_title'), 0, 28, "..."));  //Maximum 31 characters allowed in sheet title.
 
-$startdate = $this->input->get("startdate") === FALSE ? 0 : $this->input->get("startdate");
-$enddate = $this->input->get("enddate") === FALSE ? 0 : $this->input->get("enddate");
+$month = $this->input->get("month") === FALSE ? 0 : $this->input->get("month");
+$year = $this->input->get("year") === FALSE ? 0 : $this->input->get("year");
 $entity = $this->input->get("entity") === FALSE ? 0 : $this->input->get("entity");
 $children = filter_var($this->input->get("children"), FILTER_VALIDATE_BOOLEAN);
 $requests = filter_var($this->input->get("requests"), FILTER_VALIDATE_BOOLEAN);
 
-//Compute facts about dates
-if ($startdate == 0 || $enddate == 0) {
-    $start = date('Y-01-01');
-    $end = date('Y-12-31');
-    $interval = abs(strtotime($end) - strtotime($start));
-    $intervalDays = floor($interval / (60 * 60 * 24));
+//Compute facts about dates and the selected month
+if ($month == 0) {
+    $start = sprintf('%d-01-01', $year);
+    $end = sprintf('%d-12-31', $year);
+    $total_days = date("z", mktime(0,0,0,12,31,$year)) + 1;
 } else {
-    $start = $startdate;
-    $end = $enddate;
-    $interval = abs(strtotime($end) - strtotime($start));
-    $intervalDays = ($interval / (60 * 60 * 24));
+    $start = sprintf('%d-%02d-01', $year, $month);
+    $lastDay = date("t", strtotime($start));    //last day of selected month
+    $end = sprintf('%d-%02d-%02d', $year, $month, $lastDay);
+    $total_days = cal_days_in_month(CAL_GREGORIAN, $month, $year);
 }
 
-$leavesBetweenDates = getLeavesBetweenDates($this, $startdate, $enddate);
-
-$result = array();
+$types = $this->types_model->getTypes();
 $leave_requests = array();
 
-foreach ($leavesBetweenDates as $leave) {
-    $result[$leave['userid']]['userid'] = $leave['userid'];
-    $result[$leave['userid']]['firstname'] = $leave['firstname'];
-    $result[$leave['userid']]['lastname'] = $leave['lastname'];
-    $result[$leave['userid']]['type'] = $leave['type'];
-    $result[$leave['userid']]['leavetype'] = $leave['leavetype'];
-    $result[$leave['userid']]['duration'] = $leave['duration'];
+//Iterate on all employees of the entity
+$users = $this->organization_model->allEmployees($entity, $children);
+
+$result = array();
+
+foreach ($users as $user) {
+    $result[$user->id]['identifier'] = $user->identifier;
+    $result[$user->id]['firstname'] = $user->firstname;
+    $result[$user->id]['lastname'] = $user->lastname;
+    $date = new DateTime($user->datehired);
+    $result[$user->id]['datehired'] = $date->format(lang('global_date_format'));
+    $result[$user->id]['department'] = $user->department;
+    $result[$user->id]['position'] = $user->position;
+    $result[$user->id]['contract'] = $user->contract;
+    $non_working_days = $this->dayoffs_model->lengthDaysOffBetweenDates($user->contract_id, $start, $end);
+    $opened_days = $total_days - $non_working_days;
+  //If the user has selected All months
+    if ($month == 0) {
+        $leave_duration = 0;
+        for ($ii = 1; $ii <13; $ii++) {
+            $linear = $this->leaves_model->linear($user->id, $ii, $year, FALSE, FALSE, TRUE, FALSE);
+            $leave_duration += $this->leaves_model->monthlyLeavesDuration($linear);
+            $leaves_detail = $this->leaves_model->monthlyLeavesByType($linear);
+            //Init type columns
+            foreach ($types as $type) {
+                if (array_key_exists($type['name'], $leaves_detail)) {
+                    if (!array_key_exists($type['name'], $result[$user->id])) {
+                        $result[$user->id][$type['name']] = 0;
+                    }
+                    $result[$user->id][$type['name']] +=
+                            $leaves_detail[$type['name']];
+                } else {
+                    $result[$user->id][$type['name']] = '';
+                }
+            }
+        }
+        if ($requests) $leave_requests[$user->id] = $this->leaves_model->getAcceptedLeavesBetweenDates($user->id, $start, $end);
+            $work_duration = $opened_days - $leave_duration;
+    } else {
+        $linear = $this->leaves_model->linear($user->id, $month, $year, FALSE, FALSE, TRUE, FALSE);
+        $leave_duration = $this->leaves_model->monthlyLeavesDuration($linear);
+        $work_duration = $opened_days - $leave_duration;
+        $leaves_detail = $this->leaves_model->monthlyLeavesByType($linear);
+        if ($requests) $leave_requests[$user->id] = $this->leaves_model->getAcceptedLeavesBetweenDates($user->id, $start, $end);
+        //Init type columns
+        foreach ($types as $type) {
+            if (array_key_exists($type['name'], $leaves_detail)) {
+                $result[$user->id][$type['name']] = $leaves_detail[$type['name']];
+            } else {
+                $result[$user->id][$type['name']] = '';
+            }
+        }
+    }
 }
 
 $max = 0;
 $line = 2;
-$i18n = array("id", "userid", "firstname", "lastname", "type", "leavetype", "duration");
+$i18n = array("identifier", "firstname", "lastname", "datehired", "department", "position", "contract");
 foreach ($result as $user_id => $row) {
     $index = 1;
     foreach ($row as $key => $value) {
         if ($line == 2) {
             $colidx = columnName($index) . '1';
             if (in_array($key, $i18n)) {
-                $sheet->setCellValue($colidx, $key);
+                $sheet->setCellValue($colidx, lang($key));
             } else {
                 $sheet->setCellValue($colidx, $key);
             }
@@ -75,6 +118,33 @@ foreach ($result as $user_id => $row) {
         $index++;
     }
     $line++;
+    //Display a nested table listing the leave requests
+    if ($requests) {
+        if (count($leave_requests[$user_id])) {
+            $sheet->setCellValue('A' . $line, lang('leaves_index_thead_start_date'));
+            $sheet->setCellValue('B' . $line, lang('leaves_index_thead_end_date'));
+            $sheet->setCellValue('C' . $line, lang('leaves_index_thead_type'));
+            $sheet->setCellValue('D' . $line, lang('leaves_index_thead_duration'));
+            $sheet->getStyle('A' . $line . ':D' . $line)->getFont()->setBold(true);
+            $sheet->getStyle('A' . $line . ':D' . $line)->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+            $line++;
+            //Iterate on leave requests
+            foreach ($leave_requests[$user_id] as $request) {
+                $date = new DateTime($request['startdate']);
+                $startdate = $date->format(lang('global_date_format'));
+                $date = new DateTime($request['enddate']);
+                $enddate = $date->format(lang('global_date_format'));
+                $sheet->setCellValue('A' . $line, $startdate . ' (' . lang($request['startdatetype']). ')');
+                $sheet->setCellValue('B' . $line, $enddate . ' (' . lang($request['enddatetype']). ')');
+                $sheet->setCellValue('C' . $line, $request['type']);
+                $sheet->setCellValue('D' . $line, $request['duration']);
+                $line++;
+            }
+        } else {
+            $sheet->setCellValue('A' . $line, "----");
+            $line++;
+        }
+    }
 }
 
 $colidx = columnName($max) . '1';
@@ -87,5 +157,5 @@ for ($ii=1; $ii <$max; $ii++) {
     $sheet->getColumnDimension($col)->setAutoSize(TRUE);
 }
 
-$spreadsheet->exportName = 'leave_requests_'. $startdate . '_' . $enddate;
+$spreadsheet->exportName = 'leave_requests_'. $month . '_' . $year;
 writeSpreadsheet($spreadsheet);
--- local/pages/en/hour-aware-report-export.php
+++ local/pages/en/hour-aware-report-export.php
@@ -1,5 +1,12 @@
 <?php
 
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Worksheet\PageSetup;
+
 require (dirname(__DIR__).'\..\reports\hour-aware-functions.php');
 
 $this->auth->checkIfOperationIsAllowed('native_report_leaves');
@@ -20,4 +27,72 @@ if (isset($_GET['refDate']) && $_GET['refDate'] != NULL) {
     $data['refDate'] = date("Y-m-d", $_GET['refDate']);
 }
 $data['include_children'] = filter_var($_GET['children'], FILTER_VALIDATE_BOOLEAN);
-$this->load->view('reports/leaves/export', $data);
+//$this->load->view('reports/leaves/export', $data);
+
+// require_once FCPATH . "vendor/autoload.php";
+
+$spreadsheet = new Spreadsheet();
+$sheet = $spreadsheet->getActiveSheet();
+
+$sheet->setTitle(mb_strimwidth(lang('reports_export_leaves_title'), 0, 28, "..."));  //Maximum 31 characters allowed in sheet title.
+
+//$startdate = $this->input->get("startdate") === FALSE ? 0 : $this->input->get("startdate");
+//$enddate = $this->input->get("enddate") === FALSE ? 0 : $this->input->get("enddate");
+//$entity = $this->input->get("entity") === FALSE ? 0 : $this->input->get("entity");
+$children = filter_var($this->input->get("children"), FILTER_VALIDATE_BOOLEAN);
+$requests = filter_var($this->input->get("requests"), FILTER_VALIDATE_BOOLEAN);
+
+$leavesBetweenDates = getLeavesBetweenDates($this, $startdate, $enddate);
+
+$result = array();
+$leave_requests = array();
+$index = 0;
+
+foreach ($leavesBetweenDates as $leave) {
+    $result[$index]['userid'] = $leave['userid'];
+    $result[$index]['lastname'] = $leave['lastname'];
+    $result[$index]['firstname'] = $leave['firstname'];
+    $result[$index]['type'] = $leave['type'];
+    $result[$index]['leavetype'] = $leave['leavetype'];
+    $result[$index]['duration'] = $leave['duration'];
+
+    if ($requests) $leave_requests[$leave['userid']] = $this->leaves_model->getAcceptedLeavesBetweenDates($leave['userid'], $startdate, $enddate);
+    $index++;
+}
+
+$index = 0;
+$max = 0;
+$line = 2;
+$i18n = array("id", "userid", "firstname", "lastname", "type", "leavetype", "duration");
+foreach ($result as $index => $row) {
+    $columnIndex = 1;
+    foreach ($row as $key => $value) {
+        if ($line == 2) {
+            $colidx = columnName($columnIndex) . '1';
+            if (in_array($key, $i18n)) {
+                $sheet->setCellValue($colidx, $key);
+            } else {
+                $sheet->setCellValue($colidx, $key);
+            }
+            $max++;
+        }
+        $colidx = columnName($columnIndex) . $line;
+        $sheet->setCellValue($colidx, $value);
+        $columnIndex++;
+    }
+    $index++;
+    $line++;
+}
+
+$colidx = columnName($max) . '1';
+$sheet->getStyle('A1:' . $colidx)->getFont()->setBold(true);
+$sheet->getStyle('A1:' . $colidx)->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+
+//Autofit
+for ($ii=1; $ii <$max; $ii++) {
+    $col = columnName($ii);
+    $sheet->getColumnDimension($col)->setAutoSize(TRUE);
+}
+
+$spreadsheet->exportName = 'leave_requests_'. $startdate . '_' . $enddate;
+writeSpreadsheet($spreadsheet);
