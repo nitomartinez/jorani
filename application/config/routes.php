18f4fc543b2781e84dc098271be43e7041b3c910
Nito Martinez <Nito@Qindel.ES>
2019-05-22 10:22:10 +0200

    Undo changes

--- application/config/routes.php
+++ application/config/routes.php
@@ -261,9 +261,7 @@ $route['reports/balance/execute'] = 'reports/executeBalanceReport';
 $route['reports/balance/export'] = 'reports/exportBalanceReport';
 $route['reports/leaves'] = 'reports/leaves';
 $route['reports/leaves/execute'] = 'reports/executeLeavesReport';
-$route['reports/leaves/executeDates'] = 'reports/executeLeavesReportBetweenDates';
 $route['reports/leaves/export'] = 'reports/exportLeavesReport';
-$route['reports/leaves/exportDates'] = 'reports/exportLeavesReportBetweenDates';
 $route['reports'] = 'reports';
 
 //_______________________________________________
